e0dcdafbe1046ab07986a7f02e03945528921f9e
hjk9984 <hjk9984@naver.com>
2022-03-08 14:41:38 +0900

    Add Status.h

---
+++ Status.h
@@ -0,0 +1,10 @@
+#ifndef STATUS_H_
+#define STATUS_H_
+
+#define DOOR_LOCK 1         // LED RED, RGB RED
+#define DOOR_UNLOCK 2       // LED BLUE, RGB RED
+#define CAR_IN_UNLOCK 3     // LED BLUE, RGB BLUE
+#define CAR_IN_LOCK 4       // LED RED, RGB BLUE
+
+
+#endif /* STATUS_H_ */
