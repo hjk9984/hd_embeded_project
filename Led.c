579a0b40fcf4a54939af3213ef1d8320b55db953
hjk9984 <hjk9984@naver.com>
2022-03-10 10:14:26 +0900

    Update Led.c Led.h add  blink blue and term argument

--- Led.c
+++ Led.c
@@ -1,7 +1,8 @@
 #include "Led.h"
 #include "Status.h"
 
-void init_LED(void){
+void init_LED(void)
+{
     PORT10_IOCR0 &= ~((0x1F) << PC1);
     PORT10_IOCR0 &= ~((0x1F) << PC2);
 
@@ -9,71 +10,97 @@ void init_LED(void){
     PORT10_IOCR0 |= ((0x10) << PC2);
 }
 
-
-void Red_On(){      //pcl 0, ps 1
-    PORT10_OMR |= (1<<PS1);
-    PORT10_OMR &= ~(1<<PCL1);
+void Red_On()
+{ // pcl 0, ps 1
+    PORT10_OMR |= (1 << PS1);
+    PORT10_OMR &= ~(1 << PCL1);
 }
 
-void Red_Off(){     //pcl 1, ps 0
-    PORT10_OMR &= ~(1<<PS1);
-    PORT10_OMR |= (1<<PCL1);
+void Red_Off()
+{ // pcl 1, ps 0
+    PORT10_OMR &= ~(1 << PS1);
+    PORT10_OMR |= (1 << PCL1);
 }
 
-void Blue_On(){
-    PORT10_OMR |= (1<<PS2);
-    PORT10_OMR &= ~(1<<PCL2);
+void Blue_On()
+{
+    PORT10_OMR |= (1 << PS2);
+    PORT10_OMR &= ~(1 << PCL2);
 }
 
-void Blue_Off(){
-    PORT10_OMR &= ~(1<<PS2);
-    PORT10_OMR |= (1<<PCL2);
+void Blue_Off()
+{
+    PORT10_OMR &= ~(1 << PS2);
+    PORT10_OMR |= (1 << PCL2);
 }
 
-// status ¿¡ µû¶ó led Á¶Àý
-void setting_LED(int status){
-    if (status < 0 || status > 4){
-        //¾çºÒ on
+// status ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ led ï¿½ï¿½ï¿½ï¿½
+void setting_LED(int status)
+{
+    if (status < 0 || status > 4)
+    {
+        //ï¿½ï¿½ï¿½ on
         Red_On();
         Blue_On();
-        for(int i=0; i < DELAY/2; i++);
+        for (int i = 0; i < DELAY / 2; i++)
+            ;
         Red_Off();
         Blue_Off();
-        for(int i=0; i < DELAY/2; i++);
+        for (int i = 0; i < DELAY / 2; i++)
+            ;
         Red_On();
         Blue_On();
-        for(int i=0; i < DELAY/2; i++);
+        for (int i = 0; i < DELAY / 2; i++)
+            ;
         Red_Off();
         Blue_Off();
-
     }
-    else if(status == DOOR_LOCK){
-        Red_On();
-        Blue_Off();
-    }
-    else if(status == DOOR_UNLOCK){
-        Red_Off();
-        Blue_On();
-    }
-    else if(status == CAR_IN_UNLOCK){
-        Red_Off();
-        Blue_On();
-    }
-    else if(status == CAR_IN_LOCK){
+    else if (status == DOOR_LOCK)
+    {
         Red_On();
         Blue_Off();
     }
+    else if (status == DOOR_UNLOCK)
+    {
+        Red_Off();
+        Blue_On();
+    }
+    else if (status == CAR_IN_UNLOCK)
+    {
+        Red_Off();
+        Blue_On();
+    }
+    else if (status == CAR_IN_LOCK)
+    {
+        Red_On();
+        Blue_Off();
+    }
 }
 
-void blink_Red_LED(int status){
-    if(status != DOOR_LOCK)
-        return;
-
+void blink_Red_LED(int term)
+{
     Red_Off();
-    for(int i=0; i < DELAY/2; i++);
+    for (int i = 0; i < DELAY / term; i++)
+        ;
     Red_On();
-    for(int i=0; i < DELAY/2; i++);
+    for (int i = 0; i < DELAY / 2; i++)
+        ;
     Red_Off();
-    for(int i=0; i < DELAY/2; i++);
+    for (int i = 0; i < DELAY / 2; i++)
+        ;
     Red_On();
+}
+
+void blink_Blue_LED(int term)
+{
+    Blue_Off();
+    for (int i = 0; i < DELAY / term; i++)
+        ;
+    Blue_On();
+    for (int i = 0; i < DELAY / term; i++)
+        ;
+    Blue_Off();
+    for (int i = 0; i < DELAY / term; i++)
+        ;
+    Blue_On();
 }
--- Led.h
+++ Led.h
@@ -2,8 +2,8 @@
 #define LED_H_
 
 #define PORT10_BASE (0xF003B000)
-#define PORT10_IOCR0 (*(volatile unsigned int*)(PORT10_BASE + 0x10))
-#define PORT10_OMR (*(volatile unsigned int*)(PORT10_BASE + 0x04))
+#define PORT10_IOCR0 (*(volatile unsigned int *)(PORT10_BASE + 0x10))
+#define PORT10_OMR (*(volatile unsigned int *)(PORT10_BASE + 0x04))
 
 #define PC1 11
 #define PCL1 17
@@ -15,8 +15,8 @@
 #define PS2 2
 
 #define PORT02_BASE (0xF003A200)
-#define PORT02_IOCR0 (*(volatile unsigned int*)(PORT02_BASE + 0x10))
-#define PORT02_IN (*(volatile unsigned int*)(PORT02_BASE + 0x24))
+#define PORT02_IOCR0 (*(volatile unsigned int *)(PORT02_BASE + 0x10))
+#define PORT02_IN (*(volatile unsigned int *)(PORT02_BASE + 0x24))
 
 #define P0 0
 #define P1 1
@@ -29,5 +29,6 @@ void Blue_Off(void);
 
 void setting_LED(int);
 void blink_Red_LED(int);
+void blink_Blue_LED(int);
 
 #endif /* LED_H_ */
